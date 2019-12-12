14ce1fa63227d6b279cf481782d5445b3c096d90
SebastianArenas <sarenasf@alumnos.ceduc.cl>
2019-12-11 23:32:41 -0300

    mi primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PantallaComponent } from './pantalla/pantalla.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { SucursalesComponent } from './sucursales/sucursales.component';
+import { HabitacionComponent } from './habitacion/habitacion.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+ {  path: '', component: PantallaComponent, pathMatch: 'full'},
+  {  path: 'clientes', component: ClienteComponent},
+  {  path: 'empleados', component: EmpleadoComponent},
+  {  path: 'habitaciones', component: HabitacionComponent},
+  {  path: 'sucursales', component: SucursalesComponent},
+  {  path: 'pantalla', component: PantallaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PantallaComponent } from './pantalla/pantalla.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { SucursalesComponent } from './sucursales/sucursales.component';
+import { HabitacionComponent } from './habitacion/habitacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantallaComponent,
+    ClienteComponent,
+    EmpleadoComponent,
+    SucursalesComponent,
+    HabitacionComponent
   ],
   imports: [
     BrowserModule,
